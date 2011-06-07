eba5ab6e5c0ef5f892fb8c02a8883498ef9decee
Keloran <keloran@develbox.info>
2011-06-07 17:16:54 +0100

    Debug

--- screws.php
+++ screws.php
@@ -12,6 +12,7 @@ class Screws {
 	private $cClass;
 	private $cPath;
 	private $aData;
+	private $cOriginal;
 
 	/**
 	 * Screws::__construct()
@@ -28,7 +29,8 @@ class Screws {
 	 * @return
 	 */
 	private function loader($cClass) {
-		$this->cClass	= $cClass;
+		$this->cClass		= $cClass;
+		$this->cOriginal	= $cClass;
 
 		$this->definePaths();
 		$bExists = $this->checkExists();
@@ -231,6 +233,8 @@ class Screws {
 				return true;
 			}
 		}
+
+if ($this->cOriginal) { print_r($this); die(); }
 
 		return false;
 	}
