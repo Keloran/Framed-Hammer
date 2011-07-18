75e66d5dda516867aa5a03c755a892c8c489eb12
Keloran <keloran@me.com>
2011-07-18 20:38:08 +0100

    See if the footer tests can work

---
+++ tests/nails/footer/footer.incTest.php
@@ -0,0 +1,39 @@
+<?php
+require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . '../../../nails/footer/footer.inc.php';
+require_once 'PHPUnit/Framework.php';
+
+class nails_footer_FooterTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testGetInstance()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	public function testGetVersion()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	public function testGetShareThis()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	public function testGetReInvigorate()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	public function testGetGoogleAnalytics()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+
+	public function testGetFoot()
+	{
+		$this->markTestIncomplete('This test has not been implemented yet.');
+	}
+}
+
+?>
