e53ea5eb4a9ad997a1bbc712b3da01f926467b1a
git <keloran@me.com>
2011-07-28 13:11:15 +0100

    See if I can make a nicer XML reader than the current one

---
+++ nails/xml/xml.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * XML
+ *
+ * @package
+ * @author keloran
+ * @copyright Copyright (c) 2011
+ * @version $Id$
+ * @access public
+ */
+class XML {
+	public static $oXML;
+
+	private $aData;
+
+	/**
+	 * XML::__construct()
+	 *
+	 */
+	public function __construct() {
+
+	}
+
+	/**
+	 * XML::__set()
+	 *
+	 * @param string $cName
+	 * @param mixed $mValue
+	 * @return null
+	 */
+	public function __set($cName, $mValue) {
+		$this->aData[$cName] = $mValue;
+	}
+
+	/**
+	 * XML::__get()
+	 *
+	 * @param string $cName
+	 * @return mixed
+	 */
+	public function __get($cName) {
+		$mReturn = false;
+
+		if (isset($this->aData[$cName])) { $mReturn = $this->aData[$cName]; }
+
+		return $mReturn;
+	}
+
+	/**
+	 * XML::getInstance()
+	 *
+	 * @return object
+	 */
+	public static function getInstance() {
+		if (is_null(self::$oXML)) { self::$oXML = new XML(); }
+
+		return self::$oXML;
+	}
+
+	public function getElement() {
+
+	}
+
+	/**
+	 * XML::setFile()
+	 *
+	 * @param string $cFile
+	 * @param bool $bAbsolute
+	 * @param bool $bDelete
+	 * @return null
+	 */
+	public function setFile($cFile, $bAbsolute = null, $bDelete = null) {
+		//given the absolute path, could even be remote
+		if (!$bAbsolute) {
+			$cRealFile	= SITEPATH . "/" . $cFile . ".xml";
+		} else {
+			$cRealFile	= $cFile;
+		}
+
+		//set to the object
+		$this->cFile = $cRealFile;
+
+		//do we want to delete it, before making a new one, and does the file exist
+		if ($bDelete && file_exists($cRealFile)) { unlink($cRealFile); }
+
+		//create the dom object
+		$this->oDOM	= new DomDocument("1.0", "UTF-8");
+
+		//if there isnt a root set
+		if (!$this->cRoot) { $this->cRoot = "config"; }
+
+		//now if there is a file, which possibly there isnt now
+		if (file_exists($cRealFile)) {
+			$pFile		= file_get_contents($cRealFile);
+			$this->oDOM->loadXML($pFile);
+		} else {
+			if ($this->xmlns) {
+				$this->oRoot = $this->oDOM->createElementNS($this->xmlns, $this->cRoot);
+			} else {
+				$this->oRoot = $this->oDOM->createElement($this->cRoot);
+			}
+
+			$this->oDOM->appendChild($this->oRoot);
+		}
+	}
+}
