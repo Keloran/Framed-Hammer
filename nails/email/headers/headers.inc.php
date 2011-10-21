702864858330d43090236b27798773c4833f47fd
git <keloran@me.com>
2011-10-21 10:37:04 +0100

    debug

--- nails/email/headers/headers.inc.php
+++ nails/email/headers/headers.inc.php
@@ -59,7 +59,7 @@ class Email_Headers extends Email_Abstract {
 		if (isset($this->oHeaders->$cHeader)) {
 			$mHeader = $this->oHeaders->$cHeader;
 
-			printRead($mHeader);die();
+			printRead($this->oHeaders);die();
 
 			//now is that header an array, or jsut a string
 			if (is_array($mHeader)) {
