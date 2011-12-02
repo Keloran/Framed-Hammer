6ac65c6be95f05e82a6cc9c646c9f33d016557ba
Keloran <keloran@me.com>
2011-12-02 14:02:28 +0000

    fixed without doing anything

--- nails/head/meta/meta.php
+++ nails/head/meta/meta.php
@@ -85,9 +85,7 @@ class Head_Meta {
 		}
 
 		$cKeyword	.= $this->cKeywords; 
-return false;
-		$cKeywords = "<meta name=\"keywords\" content=\"" . $cKeyword . "\" />\n";
-		return $cKeywords;
+		return "<meta name=\"keywords\" content=\"" . $cKeyword . "\" />\n";
 	}
 
 	/**
