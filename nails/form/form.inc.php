54347d2bded8f162d2ca400992f79bc461e9e43c
Keloran <keloran@me.com>
2011-12-19 21:00:54 +0000

    fix the form

--- nails/form/form.inc.php
+++ nails/form/form.inc.php
@@ -1160,6 +1160,7 @@ class Form {
 	 */
 	public function fullForm($cTemplate = null) {
 		$cReturn	= false;
+		$bOpened	= false;
 
 		//this is to stop forms without buttons, searchforms can force this true
 		if (!$this->checkForButtons()) {
