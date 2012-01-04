558e7e897597fd1d47607887623f092c012529ec
Keloran <keloran@me.com>
2012-01-04 09:55:55 +0000

    remove that one

--- nails/template/abstract/abstract.php
+++ nails/template/abstract/abstract.php
@@ -169,7 +169,7 @@ abstract class Template_Abstract {
 		if ($this->cCaller == "content") {
 			printRead(array(
 				"Variables"	=> get_defined_vars(),
-				"Constants"	=> get_defined_constants(),
+				//"Constants"	=> get_defined_constants(),
 				"Functions"	=> get_defined_functions()
 			));
 			die();
