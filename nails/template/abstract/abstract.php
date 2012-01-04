2e45c5b2b68d79de583709a6ee620244f3c6a624
Keloran <keloran@me.com>
2012-01-04 14:09:15 +0000

    debug

--- nails/template/abstract/abstract.php
+++ nails/template/abstract/abstract.php
@@ -422,12 +422,12 @@ abstract class Template_Abstract {
 					if (isset($debug['args']) && isset($debug['args'][0])) {
 						$cFile = $debug['args'][0];
 						$this->addDebug("Back Trace", $debug);
-						$aDebugger['file'][] = $cFile;
 					}
 					break;
 			}
 
-			$aDebugger['function'][] = $debug['function'];
+			$aDebugger['function'][] 	= $debug['function'];
+			$aDebugger['args'][]		= $debug['args'];
 		}
 
 		//debug caller function
