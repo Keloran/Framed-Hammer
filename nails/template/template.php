e85cc3884c531332e9b52b8826a97eac3aaed8fe
Keloran <keloran@me.com>
2012-01-04 15:48:27 +0000

    interesting to see what comes out

--- nails/template/template.php
+++ nails/template/template.php
@@ -141,6 +141,9 @@ class Template extends Template_Abstract {
 	 */
 	public function getContent($cTemplate = null, $bEcho = null) {
 		if ($this->oType instanceof Template_Content) {
+			printRead(get_class($this->oType));
+			die();
+
 			$oContent	= new Template_Content_Template($this->oType);
 			$oContent->setTemplate($cTemplate);
 
@@ -148,9 +151,6 @@ class Template extends Template_Abstract {
 
 			return $oContent;
 		}
-
-		printRead($this->oType);
-		die();
 	}
 
 	/**
