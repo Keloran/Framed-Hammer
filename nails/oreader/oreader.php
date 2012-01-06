4142c6cd3459a31562579a3daec4da4d4c2aa06b
Keloran <keloran@me.com>
2012-01-06 13:10:12 +0000

    debug

--- nails/oreader/oreader.php
+++ nails/oreader/oreader.php
@@ -56,7 +56,7 @@ class oReader {
 	 */
 	private function makeConsoleLines($cString) {
 		$cString	= preg_replace("{[\t]+}", "    ", $cString);
-		$cString	= trim(preg_replace('/\s+/g', '', $cString));
+		$cString	= trim(preg_replace('/\s+/i', '', $cString));
 
 		return $cString;
 	}
