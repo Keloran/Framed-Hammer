d96973d79a5d21c98ad75a845a05de51d503b576
Keloran <maxhooton@rentalcars.com>
2012-03-15 11:27:15 +0000

    fix for border not showing

--- nails/charts/svg/create/create.php
+++ nails/charts/svg/create/create.php
@@ -74,6 +74,8 @@ class Charts_SVG_Create {
 			$iCount++;
 		}
 
+		//if kCount = 0, set to 1 so border is rendered
+		if ($kCount == 0) { $kCount = 1; }
 		$iHeight 	= (($iHeight * $kCount) + 5);
 		$iWidth		= (($iWidth * ($kCount * 2)) + 10);;
 
