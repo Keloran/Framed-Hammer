4e18d6bbb55222a636f1f575de7d757419976ca3
Keloran <keloran@me.com>
2012-04-17 21:17:30 +0100

    herm

--- nails/template/static/static.php
+++ nails/template/static/static.php
@@ -42,9 +42,10 @@ class Template_Static extends Template_Abstract {
 		$cReturn	= false;
 
 		$this->oDB->read("SELECT cPage FROM template_static WHERE cPath = ? LIMIT 1", $this->mParams['fullAddress']);
-		if ($this->oDB->nextRecord()) { $cReturn	= $this->oDB->f('cPage'); }
+		if ($this->oDB->nextRecord()) { $cReturn = $this->oDB->f('cPage'); }
 
 		printRead($this->oDB->printQuery());
+		printRead($this->oDB);
 
 		return $cReturn;
 	}
